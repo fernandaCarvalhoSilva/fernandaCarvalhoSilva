21c48fa16400c2629a08ae0e404db7a186766e7f
fernada.c.silva <fer.carvalho153@gmail.com>
2024-02-08 20:01:04 -0300

    changing route

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Portfolio />} />
+        <Route path="/resume" element={<Portfolio />} />
       </Routes>
     </>
   );
